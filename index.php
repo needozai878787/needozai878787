1a97ef5de9926a601a69ebc9f94b4e01b11a9ef9
needozai878787 <needozai878787@gmail.com>
2017-10-11 22:37:32 +0500

    lagarbagar commit 1

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require $server_dir.'scripts/config.inc.php';
 require $server_dir.'scripts/wampserver.lib.php';
 //Hello i am here.. gere//re 
 //hi its me nadeem he
-
+//new branch lagarbagar change 1
 
 //chemin jusqu'aux fichiers alias
 $aliasDir = $server_dir.'alias/';
